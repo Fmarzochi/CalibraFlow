9f39199d7d4d14e0c510f737044a99bb92300111
Felipe Marzochi <fmarzochi@gmail.com>
2026-02-14 16:46:29 -0300

    Eu criei a classe main para inicializar o Spring Boot

---
+++ backend/src/main/java/com/calibrflow/api/CalibrFlowApiApplication.java
@@ -0,0 +1,13 @@
+package com.calibrflow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalibrFlowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalibrFlowApiApplication.class, args);
+    }
+
+}
