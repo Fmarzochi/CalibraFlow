dfbcafee76cbdfde1476cd6436a37370cd9c9f4e
Felipe Marzochi <fmarzochi@gmail.com>
2026-02-14 17:33:18 -0300

    Eu implementei a entidade User com relacionamento ManyToMany para perfis

---
+++ backend/src/main/java/com/calibraflow/api/domain/entities/User.java
@@ -0,0 +1,40 @@
+package com.calibraflow.api.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "tb_users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+}
