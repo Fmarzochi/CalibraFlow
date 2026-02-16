cc3f2bd5a984975df6ddc7ae4d29aa180d550860
Felipe Marzochi <fmarzochi@gmail.com>
2026-02-15 21:57:31 -0300

    Eu implementei a entidade User com suporte a roles e autenticação

---
+++ backend/src/main/java/com/calibraflow/api/domain/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.calibraflow.api.domain.repositories;
+
+import com.calibraflow.api.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
