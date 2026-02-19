c4cf085a6590192e1ef58330d44ce56302581b63
Felipe Marzochi <fmarzochi@gmail.com>
2026-02-18 23:03:43 -0300

    Eu removi as exclusões automáticas para permitir a persistência de dados reais no banco de dados.

--- backend/src/main/java/com/calibraflow/api/config/TestConfig.java
+++ backend/src/main/java/com/calibraflow/api/config/TestConfig.java
@@ -1,7 +1,7 @@
 package com.calibraflow.api.config;
 
 import com.calibraflow.api.domain.entities.Category;
-import com.calibraflow.api.domain.repositories.*;
+import com.calibraflow.api.domain.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -16,35 +16,16 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    @Autowired
-    private InstrumentRepository instrumentRepository;
-
-    @Autowired
-    private MovementRepository movementRepository;
-
-    @Autowired
-    private CalibrationRepository calibrationRepository;
-
-    @Autowired
-    private LocationRepository locationRepository;
-
     @Override
     public void run(String... args) throws Exception {
+        if (categoryRepository.count() == 0) {
+            Category c1 = new Category(null, "Pressão", 365, "Instrumentos de medição de pressão e vácuo");
+            Category c2 = new Category(null, "Temperatura", 365, "Termômetros, termopares e controladores");
+            Category c3 = new Category(null, "Dimensional", 730, "Paquímetros, micrômetros e réguas");
+            Category c4 = new Category(null, "Massa", 365, "Balanças analíticas e industriais");
+            Category c5 = new Category(null, "Volume", 365, "Vidrarias e medidores de vazão");
 
-        movementRepository.deleteAll();
-        calibrationRepository.deleteAll();
-        instrumentRepository.deleteAll();
-        categoryRepository.deleteAll();
-        locationRepository.deleteAll();
-
-        Category c1 = new Category(null, "Pressão", 365, "Instrumentos de medição de pressão e vácuo");
-        Category c2 = new Category(null, "Temperatura", 365, "Termômetros, termopares e controladores");
-        Category c3 = new Category(null, "Dimensional", 730, "Paquímetros, micrômetros e réguas");
-        Category c4 = new Category(null, "Massa", 365, "Balanças analíticas e industriais");
-        Category c5 = new Category(null, "Volume", 365, "Vidrarias e medidores de vazão");
-
-        categoryRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5));
-
-        System.out.println(">>> CalibraFlow: Banco de dados limpo e categorias reiniciadas com sucesso!");
+            categoryRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5));
+        }
     }
 }
