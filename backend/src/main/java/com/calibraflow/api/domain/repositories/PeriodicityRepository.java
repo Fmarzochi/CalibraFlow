5263896df2d4d863839a7d6d25af990b31d648a1
Felipe Marzochi <fmarzochi@gmail.com>
2026-02-20 15:07:22 -0300

    Eu refatoro repositorio Periodicity alterando tipo do id para Long

--- backend/src/main/java/com/calibraflow/api/domain/repositories/PeriodicityRepository.java
+++ backend/src/main/java/com/calibraflow/api/domain/repositories/PeriodicityRepository.java
@@ -3,10 +3,10 @@ package com.calibraflow.api.domain.repositories;
 import com.calibraflow.api.domain.entities.Periodicity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.UUID;
+
 import java.util.Optional;
 
 @Repository
-public interface PeriodicityRepository extends JpaRepository<Periodicity, UUID> {
+public interface PeriodicityRepository extends JpaRepository<Periodicity, Long> {
     Optional<Periodicity> findByInstrumentName(String instrumentName);
 }
