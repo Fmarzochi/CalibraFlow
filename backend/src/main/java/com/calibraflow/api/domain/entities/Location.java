6f842f5f6f51b9fe31c9dc457cf890319f6175ac
Felipe Marzochi <fmarzochi@gmail.com>
2026-02-20 13:54:11 -0300

    Eu refatoro entidade Location alterando id para Long e adiciono Lombok Builder

--- backend/src/main/java/com/calibraflow/api/domain/entities/Location.java
+++ backend/src/main/java/com/calibraflow/api/domain/entities/Location.java
@@ -2,19 +2,21 @@ package com.calibraflow.api.domain.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.UUID;
 
 @Entity
 @Table(name = "tb_locations")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Location {
 
     @Id
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Column(nullable = false, unique = true)
     private String name;
