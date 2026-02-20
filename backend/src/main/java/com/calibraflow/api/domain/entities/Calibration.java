101bf4e1c6055a92fef565a3601649b23b2ae259
Felipe Marzochi <fmarzochi@gmail.com>
2026-02-20 13:59:12 -0300

    Eu refatoro entidade Calibration alterando id para Long e adiciono Lombok Builder

--- backend/src/main/java/com/calibraflow/api/domain/entities/Calibration.java
+++ backend/src/main/java/com/calibraflow/api/domain/entities/Calibration.java
@@ -1,21 +1,30 @@
 package com.calibraflow.api.domain.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
-import java.util.UUID;
 
 @Entity
 @Table(name = "tb_calibrations")
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Calibration {
+
     @Id
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private LocalDate calibrationDate;
+
     private LocalDate nextCalibrationDate;
+
     private String certificateNumber;
+
     private String laboratory;
 
     @ManyToOne
