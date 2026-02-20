a767f3eb4675ba273a51f98ae05858f787d71b4e
Felipe Marzochi <fmarzochi@gmail.com>
2026-02-20 14:15:43 -0300

    Eu refatoro repositorio Category alterando tipo do id para Long

--- backend/src/main/java/com/calibraflow/api/domain/repositories/CategoryRepository.java
+++ backend/src/main/java/com/calibraflow/api/domain/repositories/CategoryRepository.java
@@ -4,9 +4,8 @@ import com.calibraflow.api.domain.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, UUID> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
 }
