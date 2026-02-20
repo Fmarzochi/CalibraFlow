14fe51e43fb29b26c0655700255cb1755ffdb41d
Felipe Marzochi <fmarzochi@gmail.com>
2026-02-20 13:45:53 -0300

    Eu Reescrevo migracoes flyway padronizando chaves primarias para BIGSERIAL

--- backend/src/main/resources/db/migration/V2__add_periodicity_id_to_instruments.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE tb_instruments ADD COLUMN periodicity_id UUID;
-ALTER TABLE tb_instruments ADD CONSTRAINT fk_instrument_periodicity FOREIGN KEY (periodicity_id) REFERENCES tb_periodicities(id);
