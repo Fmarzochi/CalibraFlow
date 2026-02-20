c26923c7da05143242267779acba20388b546a05
Felipe Marzochi <fmarzochi@gmail.com>
2026-02-20 15:06:12 -0300

    Eu refatoro repositorio Patrimony alterando tipo do id para Long

--- backend/src/main/java/com/calibraflow/api/domain/repositories/PatrimonyRepository.java
+++ backend/src/main/java/com/calibraflow/api/domain/repositories/PatrimonyRepository.java
@@ -3,8 +3,7 @@ package com.calibraflow.api.domain.repositories;
 import com.calibraflow.api.domain.entities.Patrimony;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
-import java.util.UUID;
 
-public interface PatrimonyRepository extends JpaRepository<Patrimony, UUID> {
+public interface PatrimonyRepository extends JpaRepository<Patrimony, Long> {
     Optional<Patrimony> findByPatrimonyCode(String code);
 }
